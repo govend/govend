05168fee775e3c18853db665d161fa2618e7b631
JackSpirou <jack.spirou@me.com>
2015-03-20 21:01:01 -0500

    fixing format

--- mkpkgs.go
+++ mkpkgs.go
@@ -1,12 +1,10 @@
 // +build ignore
 
-// Command mkindex creates the file "stdpkgs.go" containing an index of the Go
-// standard library.
+// This file generates stdpkgs.go, which contains the standard library packages.
 
-// This file has been modified from is source which can be found here:
+// This file has been modified from its original source:
 // https://github.com/golang/tools/blob/master/imports/mkindex.go
 
-// To execute this file and generate stdpkgs.go, I simply "go run mkstdpkgs.go".
 package main
 
 import (
@@ -22,48 +20,80 @@ import (
 	"path/filepath"
 )
 
-var (
-	pkgIndex = make(map[string][]pkg)
-)
+// pkgs is a map of the standard golang packages.
+var pkgs = make(map[string][]pkg)
+
+// fset is a set of file tokens.
+var fset = token.NewFileSet()
+
+// pkg represents a packages.
+type pkg struct {
+	importpath string // full pkg import path, e.g. "net/http"
+	dir        string // absolute file path to pkg directory e.g. "/usr/lib/go/src/fmt"
+}
 
 func main() {
-	// Don't use GOPATH.
+
+	// start with the default context.
 	ctx := build.Default
+
+	// remove the GOPATH, we only want to search packages in the GOROOT.
 	ctx.GOPATH = ""
 
-	// Populate pkgIndex global from GOROOT.
+	// iterate through the list of package source root directories.
 	for _, path := range ctx.SrcDirs() {
+
+		// open the file
 		f, err := os.Open(path)
 		if err != nil {
 			log.Print(err)
 			continue
 		}
+
+		// grab all the child names from the directory in a single slice.
 		children, err := f.Readdir(-1)
+
+		// close the file and check for errors.
 		f.Close()
 		if err != nil {
 			log.Print(err)
 			continue
 		}
+
+		// iterate through each child name.
 		for _, child := range children {
+
+			// check the child name is a directory.
 			if child.IsDir() {
-				loadPkg(path, child.Name())
+
+				// load the package path and name.
+				load(path, child.Name())
 			}
 		}
 	}
 
-	// Construct source file.
+	// start with a byte buffer.
 	var buf bytes.Buffer
-	fmt.Fprint(&buf, pkgIndexHead)
-	fmt.Fprintf(&buf, "var stdpkgs = %#v\n", pkgIndex)
+
+	// write preliminary data to the byte buffer.
+	buf.WriteString("package main\n")
+	fmt.Fprintf(&buf, "var stdpkgs = %#v\n", pkgs)
+
+	// transfer buffer bytes to final source.
 	src := buf.Bytes()
 
-	// Replace main.pkg type name with pkg.
+	// replace main.pkg type name with pkg.
 	src = bytes.Replace(src, []byte("main.pkg"), []byte("pkg"), -1)
-	// Replace actual GOROOT with "/go".
+
+	// replace actual GOROOT with "/go".
 	src = bytes.Replace(src, []byte(ctx.GOROOT), []byte("/go"), -1)
-	// Add some line wrapping.
-	src = bytes.Replace(src, []byte("}, "), []byte("},\n"), -1)
+
+	// add line wrapping.
+	src = bytes.Replace(src, []byte("[]pkg{"), []byte("[]pkg{\n"), -1)
+	src = bytes.Replace(src, []byte(", pkg"), []byte(",\npkg"), -1)
+	src = bytes.Replace(src, []byte("}}, "), []byte("},\n},\n"), -1)
 	src = bytes.Replace(src, []byte("true, "), []byte("true,\n"), -1)
+	src = bytes.Replace(src, []byte("}}}"), []byte("},\n},\n}"), -1)
 
 	var err error
 	src, err = format.Source(src)
@@ -78,19 +108,15 @@ func main() {
 	}
 }
 
-const pkgIndexHead = `package main
-`
-
-var fset = token.NewFileSet()
-
-func loadPkg(root, importpath string) {
+// load takes a path root and import path
+func load(root, importpath string) {
 	shortName := path.Base(importpath)
 	if shortName == "testdata" {
 		return
 	}
 
 	dir := filepath.Join(root, importpath)
-	pkgIndex[shortName] = append(pkgIndex[shortName], pkg{
+	pkgs[shortName] = append(pkgs[shortName], pkg{
 		importpath: importpath,
 		dir:        dir,
 	})
@@ -113,12 +139,7 @@ func loadPkg(root, importpath string) {
 			continue
 		}
 		if child.IsDir() {
-			loadPkg(root, filepath.Join(importpath, name))
+			load(root, filepath.Join(importpath, name))
 		}
 	}
-}
-
-type pkg struct {
-	importpath string // full pkg import path, e.g. "net/http"
-	dir        string // absolute file path to pkg directory e.g. "/usr/lib/go/src/fmt"
 }
