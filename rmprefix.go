05991f2b6fdd5b1b56ff4a0b7e65579f8e885957
JackSpirou <jack.spirou@me.com>
2015-03-21 20:10:14 -0500

    fixed bug for removing internal project packages

--- rmprefix.go
+++ rmprefix.go
@@ -6,21 +6,25 @@ func rmprefix(prefix string, items []string) []string {
 	// determine the length of prefix, we only want to do this once
 	l := len(prefix)
 
+	// create an empty slice of results to fill.
+	var results []string
+
 	// iterate through the slice of items
-	for i, item := range items {
+	for _, item := range items {
 
 		// check the prefix length is less than or equal to the next item
 		// this ensures no out of bounds memory errors will occur
 		if l <= len(item) {
 
-			// check if the prefix matches the current item
-			if item[:l] == prefix {
-
-				// remove that item from the slice
-				items = append(items[:i], items[i+1:]...)
+			// check if the prefix matchs the current item
+			if prefix == item[:l] {
+				continue
 			}
 		}
+
+		// remove that item from the slice
+		results = append(results, item)
 	}
 
-	return items
+	return results
 }
---
+++ test/test.go
@@ -0,0 +1,6 @@
+package test
+
+// Test function for test package.  It returns a "test" string.
+func Test() string {
+	return "test"
+}
