ba88a6f712a32f8a42bf974286f45a0f94e69ac7
jackspirou <jack.spirou@target.com>
2015-03-28 12:28:35 -0500

    removing tag debugging prints

--- vendcmd.go
+++ vendcmd.go
@@ -226,17 +226,6 @@ func vendcmd(verbose bool) error {
 				}
 
 			RevMatch:
-
-				tags, err := r.VCS.Tags(filepath.Join(vendorTempPath, r.Root))
-				if err != nil {
-					panic(err)
-				}
-
-				for i, v := range tags {
-					fmt.Print(i)
-					fmt.Print(" = ")
-					fmt.Println(v)
-				}
 			}
 
 			// verbosity
