103922bdd818d07df9bafc2a7d46c7c93f65f6a6
Felipe Musse <felipe.musse@sap.com>
2017-01-24 15:34:56 -0200

    Creating parents of download directory when downloading repo
    This fixes problems when downloading some Bazaar repos, as
    'bzr branch' yields an error when the parent of the TO_LOCATION
    (see bzr help branch) does not exist.

--- deps/repos/download.go
+++ deps/repos/download.go
@@ -13,18 +13,26 @@ import (
 // of the repository is returned and the directory is created.
 func Download(r *Repo, dir, vendorRev string) (string, error) {
 
+	downloadDir := filepath.Join(dir, r.ImportPath)
+
 	// if that repo already exists, remove it
-	if err := os.RemoveAll(filepath.Join(dir, r.ImportPath)); err != nil {
+	if err := os.RemoveAll(downloadDir); err != nil {
+		return "", err
+	}
+
+	// create parents of the download dir, as Create() and CreateAtRev()
+	// expect it to exist
+	if err := os.MkdirAll(filepath.Dir(downloadDir), 0777); err != nil {
 		return "", err
 	}
 
 	// create the repository at a specific revision
 	if vendorRev == "" || vendorRev == "latest" {
-		if err := r.VCS.Create(filepath.Join(dir, r.ImportPath), r.URL); err != nil {
+		if err := r.VCS.Create(downloadDir, r.URL); err != nil {
 			return "", err
 		}
 	} else {
-		if err := r.VCS.CreateAtRev(filepath.Join(dir, r.ImportPath), r.URL, vendorRev); err != nil {
+		if err := r.VCS.CreateAtRev(downloadDir, r.URL, vendorRev); err != nil {
 			return "", err
 		}
 	}
